323a6df91bc1b180b497dce535ecb8ce4432d0c0
Zach Kanzler <zkanzler@perchsecurity.com>
2018-05-26 22:34:59 -0400

    Refactor Director v2 to ease strategy manipulation

--- minesweeper/director/attempt2.py
+++ minesweeper/director/attempt2.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Iterable, Tuple, Set
+from typing import Iterable, Tuple, Set, Optional
 
 from minesweeper.datastructures import PropertyGraph
 from minesweeper.director.base import Director, Cell
@@ -46,7 +46,7 @@ class GroupGraph(PropertyGraph[Group, Cell]):
 class AttemptDosDirector(Director):
     """Strategies based on a heat map of mine probabilities"""
 
-    def find_visible_groups(self) -> Group:
+    def find_visible_groups(self) -> Iterable[Group]:
         """Find groups by naively matching numbers to their unrevealed neighbours
 
         This method may return subsets or duplicates of other groups.
@@ -56,6 +56,7 @@ class AttemptDosDirector(Director):
             for cell in self.control.get_cells()
             if cell.is_unrevealed()
         }
+
         for cell in self.control.get_cells():
             if not cell.is_number():
                 continue
@@ -73,9 +74,9 @@ class AttemptDosDirector(Director):
             # Is there any more informed way to set this?
             # Does it matter, pragmatically?
             # Is there a way to refactor, such that the pragmatics match the semantics?
-            yield Group(remaining_unrevealed, self.control.get_mines_left())
+            yield Group(remaining_unrevealed, len(remaining_unrevealed) // 2)
 
-    def simplify_groups(self, groups: Iterable[Group]) -> Set[Group]:
+    def simplify_groups(self, groups: Iterable[Group]) -> Optional[Set[Group]]:
         clean = False
         graph = GroupGraph(groups)
 
@@ -86,36 +87,49 @@ class AttemptDosDirector(Director):
                 contained_groups = graph.relatives_contained_by(group)
 
                 if contained_groups:
-                    graph.remove(group)
-                    for contained in contained_groups:
-                        graph.add(group - contained)
                     clean = False
+                    graph.remove(group)
+
+                    for contained in contained_groups:
+                        split_group = group - contained
+                        if split_group.num_mines >= 0:
+                            graph.add(split_group)
+                        else:
+                            # The groups passed are untrue – they don't jive with each other
+                            return None
 
         return set(graph)
 
-    def flatten_groups(self, groups: Iterable[Group]) -> Tuple[float, Cell]:
+    def flatten_groups(self, groups: Iterable[Group]) -> Iterable[Tuple[float, Cell]]:
         graph = GroupGraph(groups)
         for cell in graph.all_props():
             containers = graph.objects_containing(cell)
             yield max(group.probability for group in containers), cell
 
+    def find_moves(self, probabilities: Iterable[Tuple[float, Cell]]
+                   ) -> Iterable[Tuple[str, Cell]]:
+        for probability, cell in probabilities:
+            if probability == 1:
+                yield 'right_click', cell
+            elif probability == 0:
+                yield 'click', cell
+
+    def exec_moves(self, moves: Iterable[Tuple[str, Cell]]):
+        for method_name, cell in moves:
+            method = getattr(cell, method_name)
+            method()
+
     def act(self):
-        groups = set(self.find_visible_groups())
-        groups = self.simplify_groups(groups)
+        groups = self.simplify_groups(self.find_visible_groups())
+        probabilities = list(self.flatten_groups(groups))
+        moves = tuple(self.find_moves(probabilities))
 
-        probabilities = tuple(self.flatten_groups(groups))
-        assert probabilities, "No unrevealed cells left!"
-
-        to_flag = tuple(c for probability, c in probabilities if probability == 1)
-        to_reveal = tuple(c for probability, c in probabilities if probability == 0)
-
-        if to_flag or to_reveal:
-            for cell in to_flag:
-                cell.right_click()
-            for cell in to_reveal:
-                cell.click()
+        if moves:
+            # Found moves with 100% confidence
+            self.exec_moves(moves)
 
         else:
-            probabilities = tuple(sorted(probabilities, key=lambda t: t[0]))
+            assert probabilities
+            probabilities.sort(key=lambda t: t[0])
             _, cell = probabilities[0]
             cell.click()
